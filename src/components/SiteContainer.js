c0cd01c001a987b026497ad4b0270591b5a70bdb
g1org11 <99177635+g1org11@users.noreply.github.com>
2023-12-06 17:51:15 +0400

    add modal

--- src/components/SiteContainer.js
+++ src/components/SiteContainer.js
@@ -16,4 +16,10 @@ const Container = styled.div`
   @media (max-width: 1000px) {
     padding: 40px 30px 80px 30px;
   }
+  @media (max-width: 744px) {
+    padding: 40px 19px 84px 19px;
+  }
+  @media (max-width: 393px) {
+    padding: 60px 30px 70px 30px;
+  }
 `;
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,16 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import { defaultTheme } from "../defaultTheme";
 import styled from "styled-components";
 import Vector from "../../assets/Vector.png";
 import Xmark from "../../assets/xmark-solid .svg";
 import Logo from "../../assets/GYM24 - logo.png";
+
 const Header = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <HeaderContainer>
       <LogoDiv>
-        <img src={Logo}></img>
+        <img src={Logo} alt="GYM24 Logo" />
       </LogoDiv>
       <MenuDiv>
+        {/* Display the burger icon by default */}
+        <BurgerButton onClick={toggleMenu} show={!showMenu}>
+          <img src={Vector} alt="Burger Icon" />
+        </BurgerButton>
+        {/* Display the X mark icon when showMenu is true */}
+        {showMenu && (
+          <XMarkButton onClick={toggleMenu} show={showMenu}>
+            <img src={Xmark} alt="X Mark Icon" />
+          </XMarkButton>
+        )}
+        {/* Display the modal when showMenu is true */}
+        {showMenu && (
+          <ModalDiv>
+            <div>
+              <ModalUl>
+                <ModalLi>
+                  <a>Advantages</a>
+                </ModalLi>
+                <ModalLi>
+                  <a>Membership</a>
+                </ModalLi>
+                <ModalLi>
+                  <a>About</a>
+                </ModalLi>
+                <ModalLi>
+                  <a>Trainers</a>
+                </ModalLi>
+              </ModalUl>
+            </div>
+          </ModalDiv>
+        )}
+        {/* Regular menu items */}
         <Ul>
           <Li>
             <a>Advantages</a>
@@ -25,40 +64,6 @@ const Header = () => {
             <a>Trainers</a>
           </Li>
         </Ul>
-        <IconsDiv>
-          <a>
-            <img src={Vector}></img>
-          </a>
-          <a>
-            <img src={Xmark}></img>
-          </a>
-        </IconsDiv>
-        <ModalDiv>
-          <div>
-            <a>
-              <img src={Vector}></img>
-            </a>
-            <a>
-              <img src={Xmark}></img>
-            </a>
-          </div>
-          <div>
-            <ModalUl>
-              <li>
-                <a>Advantages</a>
-              </li>
-              <li>
-                <a>Membership</a>
-              </li>
-              <li>
-                <a>About</a>
-              </li>
-              <li>
-                <a>Trainers</a>
-              </li>
-            </ModalUl>
-          </div>
-        </ModalDiv>
       </MenuDiv>
     </HeaderContainer>
   );
@@ -68,11 +73,34 @@ export default Header;
 
 const HeaderContainer = styled.div`
   width: 100%;
+  padding: 20px;
   color: ${defaultTheme.colors.white};
   display: flex;
   align-items: center;
   justify-content: space-between;
+  position: relative;
 `;
+
+const BurgerButton = styled.button`
+  width: 50px;
+  height: 50px;
+  display: none;
+  all: unset;
+  @media (max-width: 393px) {
+    display: ${(props) => (props.show ? "inline-block" : "none")};
+  }
+`;
+
+const XMarkButton = styled.button`
+  height: 50px;
+  width: 50px;
+  display: none;
+  all: unset;
+  @media (max-width: 393px) {
+    display: ${(props) => (props.show ? "inline-block" : "none")};
+  }
+`;
+
 const LogoDiv = styled.div`
   display: flex;
   align-items: center;
@@ -82,6 +110,9 @@ const Ul = styled.ul`
   display: flex;
   align-items: center;
   list-style-type: none;
+  @media (max-width: 393px) {
+    display: none;
+  }
 `;
 
 const Li = styled.li`
@@ -91,24 +122,41 @@ const Li = styled.li`
   line-height: normal;
   margin-right: 70px;
 `;
+
 const MenuDiv = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
 `;
+
 const IconsDiv = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
 `;
+
 const ModalUl = styled.ul`
   list-style-type: none;
   margin-bottom: 10px;
 `;
+
 const ModalDiv = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   position: absolute;
-  top: 30%;
-  left: 50%;
+  top: 100%;
+  left: 0%;
   background-color: ${defaultTheme.colors.yellow};
   padding: 20px;
+`;
+
+const ModalLi = styled.li`
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 300;
+  line-height: normal;
+  color: ${defaultTheme.colors.black};
+  margin-bottom: 8px;
 `;
